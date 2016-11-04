5fb86fe4fbe59aef1e3f3c1b7ef2a51d767f65b4
Doug Ramsay <doug@qedinvestors.com>
2016-11-04 10:06:27 -0400

    WIP - messages back and forth

--- background.js
+++ background.js
@@ -1,59 +1,31 @@
-// Listen for a click on the icon. On that click, take a screenshot.
+var screenshots = [],
+    activeTab;
 
-var screenshots = []
+// function openTargetTab(newUrl, targetId) {
+//   chrome.tabs.create({url: newUrl}, function(tab) {
+//     targetId = tab.id;
+//   });
+// }
 
-function openTargetTab(newUrl, targetId) {
-  chrome.tabs.create({url: newUrl}, function(tab) {
-    targetId = tab.id;
-  });
-}
+function captureSlides(num_slides) {
 
-function captureSlide(viewTabUrl, targetId) {
-  // alert(viewTabUrl);
-  // chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-  //   screenshots.push(screenshotUrl);
-  // });
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var id = 234933;
-  var viewTabUrl = chrome.extension.getURL('output.html?id=' + id);
-  var targetId = null;
-
-  openTargetTab(viewTabUrl, targetId);
-
-  chrome.tabs.executeScript(null, {code: "var viewer = document.getElementById('viewer'), slides = viewer.getElementsByClassName('item');"}, function(slides) {
-    for (var i = 0; i < slides.length; i++) {
-      alert('slide: ' + slides[0].innerHTML);
-      chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-        screenshots.push(screenshotUrl);
-      });
-    }
+  alert("button clicked");
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {message: "get_slides"}, function(num_slides) {
+      alert("capturing " + num_slides + " slides");
+      for (var i = 0; i < num_slides; i++) {
+        chrome.tabs.sendMessage(activeTab.id, {message: 'next_slide', slide_index: i}, function() {
+          chrome.tabs.captureVisibleTab(null, function(screenshotUrl) {
+            screenshots.splice(i, 0, screenshotUrl);
+          });
+        });
+        alert("" + i + ": " + screenshotUrl);
+      }
+      console.log(screenshots.join('\n'));
+    });
   });
-
-  // chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
-  //   // We are waiting for the tab we opened to finish loading.
-  //   // Check that the tab's id matches the tab we opened,
-  //   // and that the tab is done loading.
-  //   if (tabId != targetId || changedProps.status != "complete")
-  //     return;
-  //
-  //   // Passing the above test means this is the event we were waiting for.
-  //   // There is nothing we need to do for future onUpdated events, so we
-  //   // use removeListner to stop getting called when onUpdated events fire.
-  //   chrome.tabs.onUpdated.removeListener(listener);
-  //
-  //   // Look through all views to find the window which will display
-  //   // the screenshot.  The url of the tab which will display the
-  //   // screenshot includes a query parameter with a unique id, which
-  //   // ensures that exactly one view will have the matching URL.
-  //   var views = chrome.extension.getViews();
-  //   for (var i = 0; i < views.length; i++) {
-  //     var view = views[i];
-  //     if (view.location.href == viewTabUrl) {
-  //       view.addScreenshotUrl(screenshots.join(''););
-  //       break;
-  //     }
-  //   }
-  // });
 });
--- content.js
+++ content.js
@@ -1,24 +1,30 @@
-var viewer = document.getElementById('viewer'),
-    slides = viewer.getElementsByClassName('item');
+function getSlides() {
+  var viewer = document.getElementById('myCarousel'),
+      slides = viewer.getElementsByClassName('item');
+  return slides;
+}
 
-console.log("in content.js: " + slides.length);
-slides;
-
-// Listen for messages from the popup
-chrome.runtime.onMessage.addListener(function (msg, sender, response) {
-  // First, validate the message's structure
-  if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
-    // Collect the necessary data
-    // (For your specific requirements `document.querySelectorAll(...)`
-    //  should be equivalent to jquery's `$(...)`)
-    var domInfo = {
-      total:   document.querySelectorAll('*').length,
-      inputs:  document.querySelectorAll('input').length,
-      buttons: document.querySelectorAll('button').length
-    };
-
-    // Directly respond to the sender (popup),
-    // through the specified callback */
-    response(domInfo);
+function nextSlide(index) {
+  alert("capturing slide " + index);
+  var slides = getSlides();
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].classList.remove('active');
   }
-});
+  slides[index].classList.add('active');
+  return true;
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.message === "get_slides") {
+      alert("getting slides");
+      sendResponse(getSlides().length);
+    } else if (request.message === "next_slide") {
+      alert("next slide");
+      nextSlide(request.slide_index)
+    }
+  }
+);
+
+console.log("aloha");
+console.log(getSlides().length);
