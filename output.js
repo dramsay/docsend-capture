df8c8344a0b4c5041e74fbbbce0e958a19fb5b61
Doug Ramsay <doug@qedinvestors.com>
2016-11-05 20:49:50 -0400

    Make sure img is only 100% wide

--- output.js
+++ output.js
@@ -3,6 +3,7 @@ function addScreenshots(screenshots) {
   for (var i = 0; i < screenshots.length; i++) {
     var img = document.createElement("img");
     img.setAttribute("src", screenshots[i]);
+    img.setAttribute("style", "width: 100%");
     target.appendChild(img);
   }
 }
